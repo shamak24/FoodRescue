17e76f246dc2fb7507721f21dbabb93ccfff1d4b
Soumil Shamak <rishitroyr2535@gmail.com>
2026-08-11 03:16:37 +0530

    Application Properties updated

--- src/main/java/com/project/FoodRescue/config/SecurityConfig.java
+++ src/main/java/com/project/FoodRescue/config/SecurityConfig.java
@@ -118,7 +118,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/donations/**",
                                 "/api/claims/**"
-                        ).permitAll()
+                        ).authenticated()
 
                         .requestMatchers(
                                 "/api/auth/login",
